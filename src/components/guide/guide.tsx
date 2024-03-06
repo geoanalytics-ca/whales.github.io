4397ad4124ac1c61083b491bc83c52eafed38c96
bsmith <bsmith@hatfieldgroup.com>
2024-03-06 12:49:43 -0800

    update guide with images and content for User

--- src/components/guide/guide.tsx
+++ src/components/guide/guide.tsx
@@ -5,8 +5,21 @@ import {
     ModalContent,
     ModalFooter
  } from "@nextui-org/react";
+import Image from 'next/image';
 import { FaInfo } from "react-icons/fa";
+import { FaImage, FaMap } from "react-icons/fa";
+import { GiWhaleTail } from "react-icons/gi";
 import styled from 'styled-components';
+
+import collectionSelected from "@imgs/collection-selected.png";
+import collectionSelect from "@imgs/collection-select.png";
+import assetList from "@imgs/asset-list.png";
+import assetSelect from "@imgs/asset-select.png";
+import dateSelect from "@imgs/date-select.png";
+import exampleOutput from "@imgs/example-output.png";
+import foundItems from "@imgs/found-items.png";
+import searchButton from "@imgs/search-button.png";
+import startDateSelect from "@imgs/start-date-select.png";
 
 const GuideContainer = styled.div`
     margin: 20px;
@@ -46,61 +59,119 @@ const UserGuideModal: React.FC = () => {
         <div>
             <FaInfo size={20} onClick={openModal} />
             <Modal
-                className="z-2 w-1/2 h-2/3 overflow-y-auto" 
+                className="z-2 w-2/3 h-4/5 overflow-y-auto" 
                 size={"3xl"}
                 isOpen={modalIsOpen}
                 onClose={closeModal}
             >
-                <ModalHeader>User Guide</ModalHeader>
+                <ModalHeader>User Guide <GiWhaleTail alt='' /></ModalHeader>
                 <button onClick={closeModal}>close</button>
                 <ModalContent className='' style={{ color: "black" }}>
                     <GuideContainer>
                         <GuideTitle>Introduction</GuideTitle>
                         <GuideText>
-                            The Data Map is a web-based application that
-                            allows users to query and visualise remote
-                            sensing data from a variety of sources.
+                            The Data Map is an innovative web-based platform that
+                            empowers users to explore, query, and visualize remote
+                            sensing data from a diverse range of sources in a user-friendly environment.
+                            This guide will walk you through the key features of the application, 
+                            providing detailed instructions on how to effectively query data, 
+                            interpret results, and utilize the visualization tools available.
+                        </GuideText>
+                        <GuideText className="flex justify-center w-100">
+                            <Image src={exampleOutput} alt="Example Output" />
                         </GuideText>
                     </GuideContainer>
                     <GuideContainer>
                         <GuideSectionTitle>Query</GuideSectionTitle>
                         <GuideText>
-                            The Query section provides a way to query
-                            remote sensing data based on a number of
-                            parameters. 
+                            The Query section offers a versatile interface for querying
+                            remote sensing data. It allows users to specify various parameters for precise results. 
+                            This section will guide you through the process of setting up your query, 
+                            including selecting data sources, defining temporal constraints, 
+                            and choosing the appropriate data layers for visualization.
                         </GuideText>
                         <GuideContainer>
-                            <GuideSectionTitle>Start/End Date</GuideSectionTitle>
+                            <GuideSectionTitle>Selecting Start and End Dates</GuideSectionTitle>
                             <GuideText>
-                                Start and End Date selection provides a
-                                way to query Assets over a specific 
-                                temporal range. 
+                                The Start and End Date feature allows you to query Assets within a specific 
+                                time frame. This is particularly useful when you are interested in data from a certain period.
+                            </GuideText>
+                            <GuideText className="flex justify-center w-100">
+                                <Image src={dateSelect} alt="Date Select" />
+                            </GuideText>
+                            <GuideText>
+                                To define your time frame, click on the date field to open the date picker. 
+                                You can easily navigate through different months and years. Once you find the desired date, click on it to select. 
+                                The selected date will then be displayed in the date field.
+                            </GuideText>
+                            <GuideText className="flex justify-center w-100">
+                                <Image className="w-1/3" src={startDateSelect} alt="Start Date Select" />
                             </GuideText>
                         </GuideContainer>
                         <GuideContainer>
-                            <GuideSectionTitle>Collections</GuideSectionTitle>
+                            <GuideSectionTitle>Choosing a Collection</GuideSectionTitle>
                             <GuideText>
-                                Select the STAC Collection you want to
-                                query from.
+                                Choose the STAC Collection that you wish to query. 
+                                This selection will determine the data sources available for your query.
+                            </GuideText>
+                            <GuideText className="flex justify-center w-100">
+                                <Image src={collectionSelect} alt="Collection Selection Interface" />
+                            </GuideText>
+                            <GuideText>
+                                The image above illustrates the collection selection interface. Once you've made your selection, it will be highlighted as shown below.
+                            </GuideText>
+                            <GuideText className="flex justify-center w-100">
+                                <Image src={collectionSelected} alt="Selected Collection Highlighted" />
                             </GuideText>
                         </GuideContainer>
                         <GuideContainer>
-                            <GuideSectionTitle>Item Results</GuideSectionTitle>
+                            <GuideSectionTitle>Executing a Search</GuideSectionTitle>
                             <GuideText>
-                                Once a query has completed, a set of STAC
-                                Items will be listed for you to select from.
+                                To initiate a query of the remote data, simply click on the Search button.
+                            </GuideText>
+                            <GuideText className="flex justify-center w-100">
+                                <Image src={searchButton} alt="Illustration of Search Button" />
                             </GuideText>
                         </GuideContainer>
                         <GuideContainer>
-                            <GuideSectionTitle>Asset Visualisation</GuideSectionTitle>
+                            <GuideSectionTitle>Search Results</GuideSectionTitle>
                             <GuideText>
-                                The Assets associated to the selected Item
-                                will be listed and available for 
+                                Upon completion of a query, a list of STAC
+                                Items will be generated for your selection.
+                            </GuideText>
+                            <GuideText className="flex justify-center w-100">
+                                <Image src={foundItems} alt="Displayed Items" />
+                            </GuideText>
+                        </GuideContainer>
+                        <GuideContainer>
+                            <GuideSectionTitle>Viewing Asset Results</GuideSectionTitle>
+                            <GuideText>
+                                After selecting an Item, the associated Assets
+                                will be displayed for your further selection.
+                            </GuideText>
+                            <GuideText className="flex justify-center w-100">
+                                <Image src={assetList} alt="List of Assets" />
+                            </GuideText>
+                        </GuideContainer>
+                        <GuideContainer>
+                            <GuideSectionTitle>Visualising Assets</GuideSectionTitle>
+                            <GuideText>
+                                Once an Item is selected, its associated Assets
+                                will be displayed and ready for 
                                 visualisation.
-                                Clicking the <i className="fas fa-camera"></i> icon will 
-                                create a pop-up model of the Asset preview plot.
-                                The <i className="fas fa-map"></i> icon will visualise
-                                the remote data onto the map.
+                                {/* <GuideText>
+                                    <span style={{display: "inline-flex", alignItems: "center"}}>
+                                        <FaImage alt='' /> : This icon indicates an option to generate a pop-up model of the Asset preview plot.
+                                    </span>
+                                </GuideText> */}
+                                <GuideText>
+                                    <span style={{display: "inline-flex", alignItems: "center"}}>
+                                        <FaMap alt='' /> : Click this icon to project the remote data onto the map.
+                                    </span>
+                                </GuideText>
+                            </GuideText>
+                            <GuideText className="flex justify-center w-100">
+                                <Image src={assetSelect} alt="Asset Selection Interface" />
                             </GuideText>
                         </GuideContainer>
                     </GuideContainer>
