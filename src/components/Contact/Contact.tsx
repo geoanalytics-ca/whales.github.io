e845527bee45159f140931430464584cfa79e1bd
bsmith <bsmith@hatfieldgroup.com>
2024-03-21 17:37:38 -0700

    server side or email server reg is required

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -2,14 +2,31 @@ import React, { useState, useEffect } from 'react';
 import { Base64 } from 'js-base64';
 import {
     Link,
-    Button
+    Button,
+    Modal,
+    ModalBody,
+    useDisclosure
 } from "@nextui-org/react";
 import { MdEmail } from "react-icons/md";
-
+import { useForm, SubmitHandler } from "react-hook-form"
 
 const ContactBanner = () => {
     const [contact, setContact] = useState<string>('');
+    const {isOpen, onOpen, onOpenChange, onClose} = useDisclosure();
     const contactString: string = "c21hcnR3aGFsZV9wcm9qZWN0QGFyY3R1cy5jYQo=";
+
+    interface IFormInput {
+        firstName: string
+        lastName: string
+        email: string
+      }
+
+    const { register, handleSubmit } = useForm<IFormInput>()
+    const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+
+    const openModal = () => {
+        isOpen;
+    };
 
     useEffect(() => {
         setContact(Base64.decode(contactString));
@@ -17,10 +34,19 @@ const ContactBanner = () => {
 
     return (
         <div>
-            <Button className="h-7 z-2 bg-buttonbg">
-                <Link className='text-black text-small' href={`mailto:${contact}`}>
+            <Modal className="z-2 h-screen" size={'5xl'} isOpen={isOpen} onOpenChange={onOpenChange} onClose={onClose}>
+                <ModalBody>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                    <input {...register("firstName", { required: true, maxLength: 20 })} />
+                    <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
+                        <input type="email" placeholder="Email" />
+                        <textarea placeholder="Message" />
+                        <Button type="submit">Send</Button>
+                    </form>
+                </ModalBody>
+            </Modal>
+            <Button className="h-7 z-2 bg-buttonbg" onClick={openModal}>
                 <MdEmail size={20} /> Contact Us
-                </Link>
             </Button>
         </div>
     );
